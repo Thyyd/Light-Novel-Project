8cc98450604f8fce9abdde8acebc20f42b1ce639
Thyyd <roastingblade@gmail.com>
2026-09-18 01:49:17 +0200

    feat: Ajout de la route GET /api/volumes/:id pour récupérer les données d'un volume précis.

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import healthRoute from './API/routes/health.route.js';
 import usersRouter from './API/routes/users.routes.js';
 import seriesRouter from './API/routes/series.routes.js';
+import volumesRouter from './API/routes/volumes.routes.js';
 import { errorHandler, notFoundHandler } from './API/middlewares/errorHandler.js';
 
 const app = express();
@@ -16,6 +17,8 @@ app.use('/api', healthRoute);
 app.use('/api/users', usersRouter);
 
 app.use('/api/series', seriesRouter);
+
+app.use('/api/volumes', volumesRouter);
 
 app.use(notFoundHandler);
 app.use(errorHandler);
