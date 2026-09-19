563af6180afac48a5c58340227f23bfee27fb5d8
Thyyd <roastingblade@gmail.com>
2026-09-19 17:05:29 +0200

    feat: Désactivation de l'injection automatique des css FontAwesome afin d'éviter les conflits Tailwind ciblant ces icônes.

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { config } from '@fortawesome/fontawesome-svg-core';
 import './index.css'
 import App from './App.jsx'
+
+config.autoAddCss = false;
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
