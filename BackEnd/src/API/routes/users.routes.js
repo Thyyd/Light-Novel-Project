5ab6820ab75429739bd0dc9a32b44966ed286257
Thyyd <roastingblade@gmail.com>
2026-09-20 18:19:22 +0200

    feat: Ajout de la route GET /api/users/email pour récupérer l'email associé à l'username, afin de permettre la connexion avec Firebase

--- BackEnd/src/API/routes/users.routes.js
+++ BackEnd/src/API/routes/users.routes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import { decodeToken } from '../middlewares/decodeToken.js';
-import { registerUser } from '../controllers/users.controller.js';
+import { registerUser, getEmailByPseudo } from '../controllers/users.controller.js';
 
 const router = Router();
 
 router.post('/', decodeToken, registerUser);
+
+router.get('/email', getEmailByPseudo);
 
 export default router;
