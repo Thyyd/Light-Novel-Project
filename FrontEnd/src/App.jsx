74844ea19897eb9eae0c82d0d9498694b974d2ee
Thyyd <roastingblade@gmail.com>
2026-09-21 14:51:35 +0200

    feat: Ajout de la route /register qui appelle Register, et ajout d'un guard pour éviter d'atterrir sur les pages /login ou /register alors qu'on est connecté

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
+import RedirectIfAuthenticated from './routes/guards/RedirectIfAuthenticated';
 import Navbar from './components/navigation/Navbar';
 import Homepage from './routes/pages/Homepage';
 import Series from './routes/pages/Series';
 import Login from './routes/auth/Login';
+import Register from './routes/auth/Register';
 // ... imports restants des pages
 
 function App() {
@@ -15,7 +17,14 @@ function App() {
           <Routes>
             <Route path="/" element={<Homepage />} />
             <Route path="/series" element={<Series />} />
-            <Route path="/login" element={<Login />} />
+            <Route
+              path="/login"
+              element={<RedirectIfAuthenticated><Login /></RedirectIfAuthenticated>}
+            />
+            <Route
+              path="/register"
+              element={<RedirectIfAuthenticated><Register /></RedirectIfAuthenticated>}
+            />
             {/* Les <Route> ici */}
           </Routes>
         </BrowserRouter>
