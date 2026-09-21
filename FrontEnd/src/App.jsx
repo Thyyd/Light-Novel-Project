18aa08f5433bf46b3113df57ad604df2707b7282
Thyyd <roastingblade@gmail.com>
2026-09-21 02:34:45 +0200

    feat: Ajout des routes /series et / à l'App

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,20 +1,26 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import Navbar from './components/navigation/Navbar';
+import Homepage from './routes/pages/Homepage';
+import Series from './routes/pages/Series';
 import Login from './routes/auth/Login';
 // ... imports restants des pages
 
 function App() {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          {/* Les <Route> ici */}
-        </Routes>
-      </BrowserRouter>
-    </AuthProvider>
+    <div className="flex flex-col h-screen">
+      <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/series" element={<Series />} />
+            <Route path="/login" element={<Login />} />
+            {/* Les <Route> ici */}
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
+    </div>
   );
 }
 
